2d310793ffe389d842b5822a722a93e7d540efec
NeverEnder4 <apettigrew.wsdev@gmail.com>
2019-01-12 10:59:51 -0800

    style index page

--- index.php
+++ index.php
@@ -5,17 +5,32 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Welcome to Foofle</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
-  <div class="main-section">
-    <div class="logo-container">
-      <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="logo">
-    </div>
-    <div class="search-container">
-      <input type="text" class="search-box" name="term">
-      <input type="submit" class="search-button" value="Search">
+
+  <div class="wrapper index-page">
+
+    <div class="main-section">
+
+      <div class="logo-container">
+        <img src="assets/img/logo.png" alt="logo">
+      </div>
+
+      <div class="search-container">
+
+        <form action="search.php" method="GET">
+
+          <input type="text" class="search-box" name="term">
+          <input type="submit" class="search-button" value="Search">
+
+        </form>
+
+      </div>
 
     </div>
+
   </div>
+
 </body>
 </html>
