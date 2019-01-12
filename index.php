ae9608eff8c454cb5a82a8819f8b7ffe0f757d22
NeverEnder4 <apettigrew.wsdev@gmail.com>
2019-01-12 10:07:03 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Welcome to Foofle</title>
+</head>
+<body>
+  <div class="main-section">
+    <div class="logo-container">
+      <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="logo">
+    </div>
+    <div class="search-container">
+      <input type="text" class="search-box" name="term">
+      <input type="submit" class="search-button" value="Search">
+
+    </div>
+  </div>
+</body>
+</html>
