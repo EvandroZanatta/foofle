fcae5f9c97a040e0de93762ef9e181752a83b40a
NeverEnder4 <apettigrew.wsdev@gmail.com>
2019-01-15 15:41:56 -0800

    Add procfile for Heroku

--- config.php
+++ config.php
@@ -1,16 +1,14 @@
-<?php 
+<?php
 
-  ob_start();
+ob_start();
 
-  // try to connect to mysql database
-  try {
+// try to connect to mysql database
+try {
 
-    $con = new PDO("mysql:dbname=foofle;host=localhost", "root", "");
-    $con -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-  
-  // if unsuccessful, show error message
-  } catch(PDOExeption $e) {
-    echo "Connection failed: " . $e -> getMessage();
-  }
+    $con = new PDO("mysql:dbname=kpmmddyqrz;host=157.230.154.46:3306", "kpmmddyqrz", "23bVyAhXcH");
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
-?>
+    // if unsuccessful, show error message
+} catch (PDOExeption $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
--- search.php
+++ search.php
@@ -17,6 +17,7 @@ $page = isset($_GET["page"]) ? $_GET["page"] : 1;
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="description" content="Search the web for sites and images.">
@@ -27,62 +28,61 @@ $page = isset($_GET["page"]) ? $_GET["page"] : 1;
   <title>Welcome to Foofle</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.6/dist/jquery.fancybox.min.css" />
   <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-  <script
-  src="https://code.jquery.com/jquery-3.3.1.min.js"
-  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-  crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous"></script>
 </head>
+
 <body>
 
   <div class="wrapper">
 
-   <div class="header">
+    <div class="header">
 
-    <div class="header-content">
+      <div class="header-content">
 
-      <div class="logo-container">
-        <a href="index.php">
-          <img src="assets/img/logo.png" alt="logo">
-        </a>
+        <div class="logo-container">
+          <a href="index.php">
+            <img src="assets/img/logo.png" alt="logo">
+          </a>
+        </div>
+
+        <div class="search-container">
+
+          <form action="search.php" method="GET">
+
+            <div class="search-bar-container">
+              <input type="hidden" name="type" value="<?php echo $type ?>">
+              <input name="term" type="text" class="search-box" value="<?php echo $term ?>">
+              <button class="search-button">
+                <img src="assets/img/icons/search.png" alt="search icon">
+              </button>
+            </div>
+
+          </form>
+
+        </div>
+
       </div>
 
-      <div class="search-container">
+      <div class="tabs-container">
 
-        <form action="search.php" method="GET">
+        <ul class="tab-list">
 
-          <div class="search-bar-container">
-            <input type="hidden" name="type" value="<?php echo $type ?>">
-            <input name="term" type="text" class="search-box" value="<?php echo $term ?>">
-            <button class="search-button">
-              <img src="assets/img/icons/search.png" alt="search icon">
-            </button>
-          </div>
+          <li class="<?php echo $type == 'sites' ? 'active' : '' ?>">
+            <a href='<?php echo "search.php?term=$term&type=sites"; ?>'>Sites</a>
+          </li>
+          <li class="<?php echo $type == 'images' ? 'active' : '' ?>">
+            <a href='<?php echo "search.php?term=$term&type=images"; ?>'>Images</a>
+          </li>
 
-        </form>
+        </ul>
 
       </div>
 
     </div>
 
-    <div class="tabs-container">
-
-      <ul class="tab-list">
-
-        <li class="<?php echo $type == 'sites' ? 'active' : '' ?>">
-          <a href='<?php echo "search.php?term=$term&type=sites"; ?>'>Sites</a>
-        </li>
-        <li class="<?php echo $type == 'images' ? 'active' : '' ?>">
-          <a href='<?php echo "search.php?term=$term&type=images"; ?>'>Images</a>
-        </li>
-
-      </ul>
-
-    </div>
-
-   </div>
-
-   <div class="main-results-section">
-<?php
+    <div class="main-results-section">
+      <?php
 if ($type == "sites") {
 
     $resultsProvider = new SiteResultsProvider($con);
@@ -102,17 +102,17 @@ echo "<p class='results-count'>$numResults results found</p>";
 
 echo $resultsProvider->getResultsHtml($page, $pageSize, $term);
 ?>
-   </div>
+    </div>
 
-   <div class="pagination-container">
+    <div class="pagination-container">
 
-    <div class="page-buttons">
+      <div class="page-buttons">
 
-      <div class="page-number-container">
-        <img src="assets/img/page-start.png">
-      </div>
+        <div class="page-number-container">
+          <img src="assets/img/page-start.png">
+        </div>
 
-<?php
+        <?php
 $pagesToShow = 10;
 $numPages = ceil($numResults / $pageSize);
 $pagesLeft = min($pagesToShow, $numPages);
@@ -151,13 +151,13 @@ while ($pagesLeft != 0 && $currentPage <= $numPages) {
 
 ?>
 
-      <div class="page-number-container">
-        <img src="assets/img/page-end.png">
+        <div class="page-number-container">
+          <img src="assets/img/page-end.png">
+        </div>
+
       </div>
 
     </div>
-
-   </div>
 
   </div>
 
@@ -165,4 +165,5 @@ while ($pagesLeft != 0 && $currentPage <= $numPages) {
   <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
   <script type="text/javascript" src="assets/js/script.js"></script>
 </body>
+
 </html>
